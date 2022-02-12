82fba6a549d554de77b67ba1c4f43789aae5091e
Peruzini <assamitafael@gmail.com>
2022-02-12 17:06:26 -0300

    Adicionaro função Muda b

--- roma.js
+++ roma.js
@@ -2,3 +2,11 @@ var decodificar = document.getElementById('decodificar');
 var codificar = document.getElementById('codificar');
 var enviarB = document.getElementById('enviar');
 
+function mudaB(){
+    if(decodificar.checked){
+        botao.innertext = "Decodificar";
+    }else if(codificar.checked){
+        botao.innertext = "Codificar"
+    }
+}
+
