001fe34e29b53360911af3e6fc5e1140a0a00369
Peruzini <assamitafael@gmail.com>
2022-02-12 17:27:12 -0300

    Textar function Checa Opção do Javascript

--- roma.js
+++ roma.js
@@ -25,3 +25,57 @@ function escondeInc() {
 cesar.addEventListener('change', mostraInc);
 C64.addEventListener('change', escondeInc);
 
+var resultado = document.querySelector('p');
+var inc = parseInt(document.getElementById('incrt').value);
+console.log(inc);
+console.log(typeof inc);
+
+botao.addEventListener('click', function(event) {
+    event.preventDefault();
+    checaOpcao();
+});
+
+function CodificarCesar(mensagem) {
+    mensagem = mensagem.split("");
+    console.log(mensagem);
+    let indexAtual = mensagem.map((x) => x.charCodeAt());
+    console.log(indexAtual);
+    let indexNovo = indexAtual.map((x) => x+inc);
+    console.log(indexNovo);
+    let mensagemNova = indexNovo.map((x) => String.fromCharCode(x)).join("");
+    console.log(mensagemNova);
+    return mensagemNova;
+}
+
+function DecodificarCesar(mensagem) {
+    mensagem = mensagem.split("");
+    let indexAtual = mensagem.map((x) => x.charCodeAt())
+    let indexNovo = indexAtual.map((x) => x-inc);
+    let mensagemNova = indexNovo.map((x) => String.fromCharCode(x)).join("");
+    console.log(mensagemNova);
+    return mensagemNova;
+}
+function checaOpcao () {
+    var mensagem = document.getElementById('retorno').value;
+    console.log(mensagem);
+
+    if (code.checked) {
+        if (cesar.checked) {
+            console.log("Codificar César")
+            resultado.innerText = codeCesar(mensagem);
+        } else {
+            console.log("Codificar base64")
+            resultado.innerText = btoa(mensagem);
+        } 
+    } else if (decode.checked) {
+        if (cesar.checked) {
+            console.log("Decodificador da Base César")
+            resultado.innerText = decodeCesar(mensagem);
+        } else {
+            console.log("Decodificar da Base 64")
+            resultado.innerText = atob(mensagem);
+        } 
+    } else {
+        resultado.innerText = "Escolha um procedimento válido.";
+    }
+}
