d085014207edc80f2307d9b031796fbb8c1b37c9
Peruzini <assamitafael@gmail.com>
2022-02-12 23:12:44 -0300

    Adicionado Mostrar Incremento

--- roma.js
+++ roma.js
@@ -5,10 +5,23 @@ var botao = document.getElementById('enviar');
 
 function mudaBotao () {
     if (decode.checked) {
-        botao.innerText = "Decodificar!";
+        botao.innerText = "Decod. Mensagem!";
     } else if (code.checked) {
-        botao.innerText = "Codificar!";
+        botao.innerText = "Codificar Mensagem!";
     }
 }
 
+var cesar = document.getElementById('cesar');
+var base64 = document.getElementById('B64');
+var incremento = document.getElementById('incremento');
 
+function mostrarincremento() { 
+    incremento.style.display = "block"; 
+}
+
+function escondeincremento() {
+    incremento.style.display = "none";
+}
+
+cesar.addEventListener('change', mostrarincremento);
+base64.addEventListener('change', escondeincremento);
