53d1f04197dc7736772dbf14207c6da6fb257b8e
Peruzini <assamitafael@gmail.com>
2022-02-12 22:25:06 -0300

    Muda B

--- roma.js
+++ roma.js
@@ -1,11 +1,13 @@
-var code = document.getElementById('codico');
-var decode = document.getElementById('decodico');
-var botaoenviar = document.getElementById('enviar');
+var code = document.getElementById('code');
+var decode = document.getElementById('decode');
+var botao = document.getElementById('botaoEnviar');
 
-function mudaB () {
+
+function mudaBotao () {
     if (decode.checked) {
         botao.innerText = "Decodificar!";
     } else if (code.checked) {
         botao.innerText = "Codificar!";
     }
 }
+
